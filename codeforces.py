92da560e242de6c243953ba560f8ebb43a375942
Ravi <ravi752696@gmail.com>
2026-02-12 22:31:56 +0530

    New problems

--- codeforces.py
+++ codeforces.py
@@ -1,40 +1,47 @@
-# n 
-# s , k, m
-"""
-
-
-10
-
-2 - 2=0  <= m
-
-
-
-m is upper limit
-
-
-10, 2, 20
-
-20 = 20%11 = 9
-
-m%n ==
-
-
-
-
-"""
-
 
 
 t=int(input())
 for _ in range(t):
-    s, k, m = map(int, input().split())
-    if k>s:
-        ans = max(0,s - m%k)
-    else:
-        last = s%k
-        if(
-
+    n, m, h = map(int, input().split())
+    a=[int(i) for i in input().split(" ")]
+    diff={}
+    flag=0
+    for i in range(m):
+        bi,ci = map(int,6+ input().split())
+        if bi-1 in  diff:
+            sum = a[bi-1] + diff[bi-1] + ci 
             
-        )
-    
+    for i in range(n):
+        print(a[i],end=" ")
+    print()
+
+
+
+
+"""
+t=int(input())
+for _ in range(t):
+    n, h, l = map(int, input().split())
+    a=[int(i) for i in input().split(" ")]
+    count=0
+    a.sort()
+    left=0
+    right=n-1
+    while(left<right):
+        if a[left]<=h and a[right]<=l:
+            left+=1
+            right-=1
+            count+=1
+        elif a[left]<=l and a[right]<=h:
+            left+=1
+            right-=1
+            count+=1
+        elif a[right]>h:
+            right-=1
+        elif a[right]>l:
+            right-=1
+
+    print(count)
+
+"""    
     
